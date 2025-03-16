90ce7084e9325959009a9072bba688e2e0f65b51
verol14 <nyseam14@gmail.com>
2025-03-16 11:43:39 +0800

    updated database schema

--- recruitmentagency.sql
+++ recruitmentagency.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2025 at 12:46 PM
+-- Generation Time: Mar 16, 2025 at 04:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,6 +39,13 @@ CREATE TABLE `applications` (
   `employer_decision` enum('pending','approved','rejected') DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `applications`
+--
+
+INSERT INTO `applications` (`application_id`, `job_id`, `seeker_id`, `status`, `applied_at`, `employer_decision`) VALUES
+(1, 2, 6, 'hired', '2025-03-09 04:45:31', 'approved');
+
 -- --------------------------------------------------------
 
 --
@@ -63,9 +70,15 @@ CREATE TABLE `application_documents` (
 DROP TABLE IF EXISTS `employers`;
 CREATE TABLE `employers` (
   `employer_id` int(11) NOT NULL,
-  `company_name` varchar(255) NOT NULL,
-  `verification_status` enum('pending','verified','rejected') DEFAULT 'pending'
+  `company_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employers`
+--
+
+INSERT INTO `employers` (`employer_id`, `company_name`) VALUES
+(7, 'Softwares inDeed');
 
 -- --------------------------------------------------------
 
@@ -82,6 +95,17 @@ CREATE TABLE `employer_documents` (
   `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `employer_documents`
+--
+
+INSERT INTO `employer_documents` (`document_id`, `employer_id`, `document_type`, `document_path`, `uploaded_at`) VALUES
+(1, 7, 'business_permit', 'uploads/employers/7/business_permit/couch.jpg', '2025-03-09 02:16:15'),
+(2, 7, 'sec_dti_registration', 'uploads/employers/7/sec_dti_registration/bed.jpg', '2025-03-09 02:16:15'),
+(3, 7, 'tin', 'uploads/employers/7/tin/din3.webp', '2025-03-09 02:16:15'),
+(4, 7, 'bir_certificate', 'uploads/employers/7/bir_certificate/din2.jpg', '2025-03-09 02:16:15'),
+(5, 7, 'official_documents', 'uploads/employers/7/official_documents/David End time.png', '2025-03-09 02:16:15');
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +120,13 @@ CREATE TABLE `feedback` (
   `comments` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedback_id`, `user_id`, `rating`, `comments`, `created_at`) VALUES
+(1, 6, 4, 'adsadad', '2025-03-15 02:44:04');
 
 -- --------------------------------------------------------
 
@@ -112,6 +143,13 @@ CREATE TABLE `interviews` (
   `notes` text DEFAULT NULL,
   `recommendation` enum('recommended','not recommended') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `interviews`
+--
+
+INSERT INTO `interviews` (`interview_id`, `application_id`, `scheduled_date`, `status`, `notes`, `recommendation`) VALUES
+(1, 1, '2025-03-12 08:00:00', 'completed', 'dsadasdasd', 'recommended');
 
 -- --------------------------------------------------------
 
@@ -132,6 +170,13 @@ CREATE TABLE `job_offers` (
   `salary` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `job_offers`
+--
+
+INSERT INTO `job_offers` (`offer_id`, `application_id`, `offer_details`, `status`, `employer_id`, `seeker_id`, `job_id`, `created_at`, `salary`) VALUES
+(1, 1, 'DaDSADas', 'accepted', 7, 6, 2, '2025-03-15 01:10:02', 20000.00);
+
 -- --------------------------------------------------------
 
 --
@@ -146,11 +191,19 @@ CREATE TABLE `job_postings` (
   `description` text NOT NULL,
   `requirements` text NOT NULL,
   `skills` text NOT NULL,
-  `status` enum('pending','approved','rejected','unavailable') DEFAULT 'pending',
+  `status` enum('pending','approved','rejected','unavailable') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `location` varchar(255) DEFAULT NULL,
   `quota` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `job_postings`
+--
+
+INSERT INTO `job_postings` (`job_id`, `employer_id`, `title`, `description`, `requirements`, `skills`, `status`, `created_at`, `location`, `quota`) VALUES
+(2, 7, 'Software Engineer', 'Magaling sa javascript at node.js', 'Certificate of candidacy', 'JavaScript, PostGreSQL, Node.js', 'unavailable', '2025-03-09 04:01:35', 'Taguig', -1),
+(3, 7, 'Senior Web Developer', 'DSADA', 'dsaDSDAD', 'DSada, JAJDISUAD', 'approved', '2025-03-15 02:24:31', 'Taguig, Philippines', 4);
 
 -- --------------------------------------------------------
 
@@ -166,6 +219,13 @@ CREATE TABLE `job_seekers` (
   `location` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `job_seekers`
+--
+
+INSERT INTO `job_seekers` (`seeker_id`, `resume_image`, `skills`, `location`) VALUES
+(6, '', NULL, 'Taguig');
+
 -- --------------------------------------------------------
 
 --
@@ -180,6 +240,17 @@ CREATE TABLE `job_seeker_documents` (
   `document_path` varchar(255) NOT NULL,
   `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `job_seeker_documents`
+--
+
+INSERT INTO `job_seeker_documents` (`document_id`, `seeker_id`, `document_type`, `document_path`, `uploaded_at`) VALUES
+(1, 6, 'valid_id', 'uploads/job_seekers/6/valid_id/1x1 formal.jpg', '2025-03-08 23:54:54'),
+(2, 6, 'tin', 'uploads/job_seekers/6/tin/bed.jpg', '2025-03-08 23:54:54'),
+(3, 6, 'resume', 'uploads/job_seekers/6/resume/bed2.webp', '2025-03-08 23:54:54'),
+(4, 6, 'photo', 'uploads/job_seekers/6/photo/co2.jpg', '2025-03-08 23:54:54'),
+(5, 6, 'qualification', 'uploads/job_seekers/6/qualification/couch.jpg', '2025-03-08 23:54:54');
 
 -- --------------------------------------------------------
 
@@ -197,6 +268,14 @@ CREATE TABLE `notifications` (
   `interview_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`notification_id`, `user_id`, `message`, `status`, `created_at`, `interview_id`) VALUES
+(3, 7, 'A candidate has been recommended for your job posting. Please review and decide whether to send a job offer.', 'read', '2025-03-14 23:53:57', 1),
+(4, 6, 'You have received a job offer (Offer ID: 1). Please respond.', 'read', '2025-03-15 01:10:02', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -213,6 +292,16 @@ CREATE TABLE `users` (
   `status` enum('pending','verified','rejected') DEFAULT 'pending',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `full_name`, `email`, `password_hash`, `user_type`, `status`, `created_at`) VALUES
+(3, 'Vincent Borja', 'vinceerolborja@gmail.com', '$2y$10$AfXVsZyaG/8kjiQtkH9laes4ggZvq./IiR99jOHBNOxATXcNtKUiy', 'employer', 'pending', '2025-03-08 05:05:35'),
+(4, 'Admin User', '123@admin.com', '$2y$10$feW8LwcOaliJbQTAIJ0N9ekjbe76ORi9xs8IGZoDS.FjKchiKF9sC', 'admin', 'verified', '2025-03-08 05:08:36'),
+(6, 'Vincent Borja', 'zeillan14@gmail.com', '$2y$10$eRRLtJLM8GCQNwWWZJvu0uSTKT4WFHliDNdHMagcxouCYV6U1ZXFS', 'job_seeker', 'verified', '2025-03-08 23:54:54'),
+(7, 'Vincent Borja', 'user123@gmail.com', '$2y$10$xjQOzffbCcrhnEigR7MBy./2V/P3Zd/cst1PdIEhFgMFe7k3Gotvu', 'employer', 'verified', '2025-03-09 02:16:15');
 
 --
 -- Indexes for dumped tables
@@ -313,7 +402,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `application_documents`
@@ -325,49 +414,49 @@ ALTER TABLE `application_documents`
 -- AUTO_INCREMENT for table `employer_documents`
 --
 ALTER TABLE `employer_documents`
-  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `interviews`
 --
 ALTER TABLE `interviews`
-  MODIFY `interview_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `interview_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `job_offers`
 --
 ALTER TABLE `job_offers`
-  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `job_postings`
 --
 ALTER TABLE `job_postings`
-  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `job_seeker_documents`
 --
 ALTER TABLE `job_seeker_documents`
-  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -442,7 +531,12 @@ ALTER TABLE `job_seeker_documents`
 --
 ALTER TABLE `notifications`
   ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `notifications_ibfk_3` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `notifications_ibfk_4` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `notifications_ibfk_5` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `notifications_ibfk_6` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `notifications_ibfk_7` FOREIGN KEY (`interview_id`) REFERENCES `interviews` (`interview_id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
